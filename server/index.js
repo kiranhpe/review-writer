04cbb1a78b4e5a641921ca9bce6f79fa7e38cbf2
kirantatkal1 <kirantatkal@gmail.com>
2021-10-23 19:41:35 +0530

    client

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+  res.json({ message: "Hello from server! Baby" });
 });
 
 // All other GET requests not handled before will return our React app
